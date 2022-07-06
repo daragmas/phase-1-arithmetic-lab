912174bdcdb6813c971e3db95f0ade291d9a309a
daragmas <daragmas@gmail.com>
2022-07-05 22:18:05 -0400

    commit tree c1bf4a8a8c9322a62cf476c35920667595f1a2bc

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1=31
+const num2=2
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = ((Math.random())*10)
+random = Math.floor(random)
+
+const num3 = 17
+const num4 = 13
+
+let mod =num3 % num4
+
+let max = Math.max(1,2,3,20)
